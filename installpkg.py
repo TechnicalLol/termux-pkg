cb9f6addc1f9cf4fa61d0bdac94ffa4b96dee4e3
sallu76626 <sallu76626@yahoo.com>
2020-04-21 02:09:36 +0530

    MALIK commit

---
+++ installpkg.py
@@ -0,0 +1,15 @@
+alik.allpkginstall
+pkg update
+pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+pkg install pip
+pkg install pip2
+pkg install php
+pkg install git
+pkg install wget
+pkg install curl
+pip2 install requests
+pip2 install mechanize
+
